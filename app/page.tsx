bd7c04011eb49ca55986202b34c1a4b019c9bb18
Rodrigo Belarmino <rb.belarmino@gmail.com>
2024-08-11 21:28:27 -0300

    feat: add header component and benner

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,32 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
   return (
-    <Button>Click me</Button>
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Ola, Rodrigo</h2>
+        <p>Domingo, 11 de Agosto</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Search" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende com FWS Barber"
+            src="/banner-01.png"
+            fill
+            className="object-cover"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 

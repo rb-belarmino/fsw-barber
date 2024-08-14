7cc115a10273f957ffde0236b48e8c7e681c15b0
Rodrigo Belarmino <rb.belarmino@gmail.com>
2024-08-13 23:12:42 -0300

    feat: add barbershop item components

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,12 @@ import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar } from "./_components/ui/avatar"
 import { AvatarImage } from "@radix-ui/react-avatar"
+import { db } from "./_lib/prisma"
+import BarberShopItem from "./_components/barbershop-item"
 
-const Home = () => {
+const Home = async () => {
+  const barbershops = await db.barbershop.findMany({})
+
   return (
     <div>
       <Header />
@@ -18,7 +22,7 @@ const Home = () => {
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Search" />
           <Button>
-            <SearchIcon />
+            <SearchIcon size={12} />
           </Button>
         </div>
         <div className="relative mt-6 h-[150px] w-full">
@@ -29,8 +33,10 @@ const Home = () => {
             className="object-cover"
           />
         </div>
-
-        <Card className="mt-6">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
           <CardContent className="flex justify-between p-0">
             <div className="flex flex-col gap-2 py-5 pl-5">
               <Badge className="w-fit">Confirmado</Badge>
@@ -49,6 +55,14 @@ const Home = () => {
             </div>
           </CardContent>
         </Card>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   )
